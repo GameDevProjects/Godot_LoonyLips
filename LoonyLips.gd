743b49eee6460c6497f88777607a2c1c248d8b3f
Martin Schmiedel <martin.schmiedel@rohde-schwarz.com>
2018-07-13 14:14:08 +0200

    Third lecture textbox and lineedit and button

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -2,5 +2,14 @@ extends Node2D
 
 func _ready():
 	var promt = [5, "Martin", "Bulgarien", "Kind", "Wölfen", 2]
-	var story = "Vor %s Jahren wanderte %s über eine Brücke mitten in %s. Unter dieser Brücke versteckte sich ein %s um sich vor %s zu verstecken es versteckte sich bereits %s Tage dort."
-	print(story % promt)
+	var story = "Vor %s Jahren wanderte %s über eine Brücke mitten in %s. Unter dieser Brücke versteckte sich ein %s um sich vor %s zu verstecken, versteckte sich bereits %s Tage dort."
+	$BlackBoard/StoryText.text = (story % promt)
+
+func _on_TextBox_text_entered(new_text):
+	$BlackBoard/StoryText.text = new_text
+	$BlackBoard/TextBox.text = ""
+
+
+func _on_TextureButton_pressed():
+	var new_text = $BlackBoard/TextBox.text
+	_on_TextBox_text_entered(new_text)
