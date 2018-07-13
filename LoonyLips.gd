baaed4f4cafeb66dd03f5c6eebca12d0e2d8bb1f
Martin Schmiedel <martin.schmiedel@rohde-schwarz.com>
2018-07-13 15:25:39 +0200

    temp changes

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -1,15 +1,21 @@
 extends Node2D
+var player_words = []
+var promt = ["eine Zahl", "eine Nummer", "ein Land", "ein Lebewesen", "weiter Lebewesen(mehrzahl)", "eine Zahl"]
+var story = "Vor %s Jahren wanderte %s über eine Brücke mitten in %s. Unter dieser Brücke schlief ein %s um sich vor %s zu verstecken, versteckte sich bereits %s Tage dort."
 
 func _ready():
-	var promt = [5, "Martin", "Bulgarien", "Kind", "Wölfen", 2]
-	var story = "Vor %s Jahren wanderte %s über eine Brücke mitten in %s. Unter dieser Brücke versteckte sich ein %s um sich vor %s zu verstecken, versteckte sich bereits %s Tage dort."
-	$BlackBoard/StoryText.text = (story % promt)
+	$BlackBoard/TextBox.text = ""
+	promt_player()
 
 func _on_TextBox_text_entered(new_text):
-	$BlackBoard/StoryText.text = new_text
+	player_words.append(new_text)
 	$BlackBoard/TextBox.text = ""
+	promt_player()
 
 
 func _on_TextureButton_pressed():
 	var new_text = $BlackBoard/TextBox.text
 	_on_TextBox_text_entered(new_text)
+	
+func promt_player():
+	$BlackBoard/StoryText.text = ("Bitte gib mir " + promt[player_words.size()] + "!")
